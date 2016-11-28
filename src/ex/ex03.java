da3a8400f59a6a90a396b8872208e65412c17e9e
Yingyin <105791120@live.asia.edu.tw>
2016-11-28 15:50:19 +0800

    ex03  105791120 杨钰莹

--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -5,11 +5,49 @@
  * Author: 1050210XX 周永振老師
  */
 
+import java.util.Scanner;
+
 public class ex03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sca=new Scanner(System.in);
+		int n=sca.nextInt();
+		int arr1[]=new int[1000];
+		int arr2[]=new int[1000];
+		int len1=1,len2=0;
+		arr1[0]=1;
+		for(int i=2;i<=n;i++){
+			if(n%i==0)
+			{
+				arr1[len1++]=i;
+				if(Judge(i))
+					arr2[len2++]=i;
+			}
+		}
+		System.out.println("因數");
+		for(int i=0;i<len1;i++)
+		{
+			System.out.println(arr1[i]+"");
+		}
+		System.out.println("\n質數：");
+		for(int i=0;i<len2;i++)
+		{
+			System.out.println(arr1[i]+"");
+		}
 	}
 
-}
+	private static boolean Judge(int x) {
+	  int b=(int)Math.sqrt((double)x);
+	  int i;
+	  for(i=2;i<=b;i++)
+	  {
+		  if(x%i==0)
+			  break;
+	  }
+	  if(i>=b+1)
+		return true;
+	  else
+		  return false;
+	}
+
+	}
